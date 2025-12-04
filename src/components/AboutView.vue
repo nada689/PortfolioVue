ff17bfe38dde0711da1a09e4588f6025e424b104
nada689 <120287148+nada689@users.noreply.github.com>
2025-12-04 16:27:40 +0200

    Update AboutView.vue

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -35,7 +35,7 @@
                 color: var(--fourth-color);
               "
               variant="text"
-              href="https://drive.google.com/file/d/15_qNe7wRG_4OdXXDjPK9racRdZatJupD/view?usp=sharing"
+              href="https://drive.google.com/file/d/1yxcv4vtCc-2o98YnIkZa8S1kEhTSm8yG/view?usp=drivesdk"
               >My CV</v-btn
             >
           </v-card-text>
